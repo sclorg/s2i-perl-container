70f25f9ce3d4afb7c0de8e035291e8513c186c41
Petr "Stone" Hracek <phracek@redhat.com>
2021-02-08 11:09:43 +0100

    Test examples from each version is a symlink into main tests
    Each example in version directory is symlinked into
    tests directory
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- 5.26/test/psgi-variables/application2.psgi
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env plackup
-use Web::Paste::Simple;
-Web::Paste::Simple->new->app;
--- 5.30/test/warningonstderr/index.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-print "Content-type: text/plain\n\n";
-warn "Warning on stderr\n";
-print "Text in HTTP body\n";
