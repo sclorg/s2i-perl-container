afd35accac81f61a6b9cb56c2503894a58a79b12
Petr "Stone" Hracek <phracek@redhat.com>
2024-08-09 12:37:26 +0200

    Copy 5.40 -> 5.38 back
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ 5.38/test/examples/from-dockerfile/5.26/Dockerfile
@@ -0,0 +1,18 @@
+FROM quay.io/centos7/perl-526-centos7
+
+# Add application sources
+ADD app-src \.
+
+# Install dependencies  cpanfile required
+RUN cpanm --notest -l extlib Module::CoreList && \
+    cpanm --notest -l extlib --installdeps .
+
+RUN printf '\
+<Location />\n\
+SetHandler perl-script\n\
+PerlResponseHandler Plack::Handler::Apache2\n\
+PerlSetVar psgi_app app.psgi\n\
+</Location>\n' > /opt/app-root/etc/httpd.d/40-psgi.conf
+
+# Run script uses standard ways to run the application
+CMD exec httpd -C 'Include /opt/app-root/etc/httpd.conf' -D FOREGROUND
---
+++ 5.38/test/test-lib-perl.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Functions for tests for the Perl image in OpenShift.
+#
+# IMAGE_NAME specifies a name of the candidate image used for testing.
+# The image has to be available before this script is executed.
+#
+
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+
+source "${THISDIR}/test-lib.sh"
+source "${THISDIR}/test-lib-openshift.sh"
+
+# Check the imagestream
+function test_perl_imagestream() {
+
+  # perl version in ./imagestreams/perl-rhel.json is either 5.26 or 5.30
+  # There is not version like `{5.26,5.30}-mod_fcgid`
+  if [ "${VERSION}" == "5.26-mod_fcgid" ]; then
+    VERSION="5.26"
+  fi
+  echo "Testing perl imagestream application"
+  ct_os_test_image_stream_quickstart "${THISDIR}/imagestreams/perl-${OS%[0-9]*}.json" \
+                                     "${THISDIR}/sample-test-app.json" \
+                                     "${IMAGE_NAME}" \
+                                     perl \
+                                     "Everything is OK" \
+                                     8080 http 200 "-p SOURCE_REPOSITORY_REF=master -p VERSION=${VERSION} -p NAME=perl-testing"
+}
+
+function test_perl_s2i_sample_app() {
+  # TODO: We should ideally use a local directory instead of ${VERSION}/test/sample-test-app,
+  # so we can test changes in that example app that are done as part of the PR
+  ct_os_test_s2i_app ${IMAGE_NAME} "https://github.com/sclorg/s2i-perl-container.git" "${VERSION}/test/sample-test-app" "Everything is OK"
+}
+
+function test_perl_s2i_dancer_app() {
+  ct_os_test_s2i_app ${IMAGE_NAME} "https://github.com/sclorg/dancer-ex.git" . 'Welcome to your Dancer application on OpenShift'
+}
+
+function test_perl_s2i_templates() {
+  # TODO: this was not working because the referenced example dir was added as part of this commit
+  ct_os_test_template_app "${IMAGE_NAME}" \
+                        "${THISDIR}/examples/templates/sample-test-app.json" \
+                        perl \
+                        "Everything is OK" \
+                        8080 http 200 \
+                        "-p SOURCE_REPOSITORY_REF=master -p VERSION=${VERSION} -p NAME=perl-testing"
+}
+
+function test_latest_imagestreams() {
+  info "Testing the latest version in imagestreams"
+  # Switch to root directory of a container
+  pushd "${THISDIR}/../.." >/dev/null
+  ct_check_latest_imagestreams
+  popd >/dev/null
+}
+
+# vim: set tabstop=2:shiftwidth=2:expandtab:
+
