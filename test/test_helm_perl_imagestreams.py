9a141921e799584742a3897d67c81945822045d8
Petr "Stone" Hracek <phracek@redhat.com>
2024-09-09 15:19:03 +0200

    Add support testig helm charts in Perl container
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_helm_perl_imagestreams.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+import pytest
+
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+from container_ci_suite.utils import check_variables
+
+if not check_variables():
+    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
+    sys.exit(1)
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmRHELPerlImageStreams:
+
+    def setup_method(self):
+        package_name = "perl-imagestreams"
+        path = test_dir
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/redhat"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("5.32-ubi9", "registry.redhat.io/ubi9/perl-532:latest"),
+            ("5.32-ubi8", "registry.redhat.io/ubi8/perl-532:latest"),
+            ("5.26-ubi8", "registry.redhat.io/ubi8/perl-526:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+
+
+class TestHelmCentOSPerlImageStreams:
+
+    def setup_method(self):
+        package_name = "perl-imagestreams"
+        path = test_dir / "../charts/centos"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("5.32-ubi9", "registry.access.redhat.com/ubi9/perl-532:latest"),
+            ("5.32-ubi8", "registry.access.redhat.com/ubi8/perl-532:latest"),
+            ("5.26-ubi8", "registry.access.redhat.com/ubi8/perl-526:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
